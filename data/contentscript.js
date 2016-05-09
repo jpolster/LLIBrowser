3131eaa306a2e394862b300ab5c864599e55aada
Urmikl18 <pavel.shevchenko.95@gmail.com>
2016-05-09 09:58:28 +0200

    The Close button

--- data/contentscript.js
+++ data/contentscript.js
@@ -8,15 +8,15 @@ else
   var div = document.createElement('div');
   div.id='mainplugindiv';
   div.style.width = '100%';
-    div.style.height = '150px';
-    div.style.background = '#313192';
-    div.style.color = 'white';
-    div.innerHTML="<script type=\"text/javascript\" src="+self.options.angularLib+"></script>"+
+  div.style.height = '150px';
+  div.style.background = '#313192';
+  div.style.color = 'white';
+  div.innerHTML="<script type=\"text/javascript\" src="+self.options.angularLib+"></script>"+
     "<script type=\"text/javascript\" src="+self.options.angularApp+"></script>"+
       "<form style=\"margin: 0 auto; left: 10px; top: 50px; width: 98%;align: middle; visibility: visible\">"+
         "<label for=\"video\" style=\"color: white\"> <input type=\"checkbox\" checked=\"checked\" name=\"video\" value=\"video\" id=\"video\" /> Video</label>"+
         "<label for=\"forum\" style=\"color: white\"> <input type=\"checkbox\" checked=\"checked\" name=\"forum\" value=\"forum\" id=\"forum\" /> Forum</label>"+
-        "<img src="+self.options.closeImg+" alt=\"Help\" style=\"width:20px;height:20px;\" align=right>"+
+        "<img src="+self.options.closeImg+" alt=\"Help\" style=\"width:20px;height:20px;\" align=right onclick=\"var youSure=confirm('Are you sure you want to close the plugin?'); if (youSure){var element = document.getElementById('mainplugindiv');element.outerHTML = '';delete element;}\">"+
         "<img src="+self.options.helpImg+" alt=\"Close\" style=\"width:20px;height:20px;\" align=right onclick=\"alert('LLIBrowser')\">"+
       "</form>"+
       "<div style=\"margin: 0 auto; left: 10px; top: 50px; width: 98%; height: 50%; align: middle; background-color:#75bbf7; visibility: visible\">"+
@@ -35,5 +35,5 @@ else
       "<a href=\"#video\" style=\"color: white; background-color: #75bbf7; height: 15px; padding: 5 auto\" ng-click=\"tab = 1\">Video</a>"+
       "<a href=\"#forum\" style=\"color: white; background-color: #313192; height: 15px; padding: 5 auto\" ng-click=\"tab = 2\">Forum</a>"+
       "</form>";
-    document.body.insertBefore(div,document.body.firstChild);
+  document.body.insertBefore(div,document.body.firstChild);
 }
