60a8f3d715aef5bdb17113975bcb8e4114000951
Urmikl18 <pavel.shevchenko.95@gmail.com>
2016-07-17 12:29:09 +0200

    Documentation is updated

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 /**
  *@file Entrance point for the Add-On
  *@module index.js
+ *@requires sdk/ui/button/action
+ *@requires sdk/tabs
+ *@requires sdk/self.data
  */
 
-var { ActionButton } = require("sdk/ui/button/action");
-var tabs = require("sdk/tabs");
-var data = require("sdk/self").data;
+ var { ActionButton } = require("sdk/ui/button/action");
+ var tabs = require("sdk/tabs");
+ var data = require("sdk/self").data;
 
 /**
  *@var button
  *@type ActionButton
  *@desc Add-On start button
  */
-var button = ActionButton({
-    id: "my-button",
-    label: "Start LLIBrowser",
-    icon: {
-      "16": "./img/logo-16.png"
-    },
-    onClick: showPlugIn
-  });
+ var button = ActionButton({
+  id: "my-button",
+  label: "Start LLIBrowser",
+  icon: {
+    "16": "./img/logo-16.png"
+  },
+  onClick: showPlugIn
+});
 
 /**
  *@function showPlugIn
  *@param state
- *@returns Opens the add-on in a new tab with the current page URL as a parameter
-*/
+ *@desc Opens the add-on page from package in a new tab with the current page URL as a parameter
+ */
 
-function showPlugIn(state){	
-  // let currUrl = tabs.activeTab.url;
+ function showPlugIn(state){	
   // var file = "file:///home/urmikl18/Test/SoftwareProject/LLIStabil/data/overlay.html";
-  // //var file = "http://www-e.uni-magdeburg.de/jpolster/LLIBrowser/overlay.html";
-  // //var file=data.url("overlay.html");
-  // //console.log(file);
-  // var completePath = file.concat("?url=").concat(currUrl);
-
-  // tabs.open(completePath);
-
+  //var file = "http://www-e.uni-magdeburg.de/jpolster/LLIBrowser/overlay.html";
+  
+  var currURL = "";
   if (tabs.activeTab.url == "about:blank" || tabs.activeTab.url == "about:newtab")
-  {tabs.open({
-    url: data.url("overlay.html").concat("?url=").concat(data.url("dummy.html").replace("://","PSZP//")),
+    currURL=data.url("dummy.html").replace("://","PSZP//");
+  else
+    currURL=tabs.activeTab.url.replace("://","PSZP//");
+
+  tabs.open({
+    url: data.url("overlay.html").concat("?url=").concat(currURL),
     inBackground: false,
     onReady: function(tab)
     {
@@ -48,16 +50,4 @@ function showPlugIn(state){
       });
     }
   }); 
-}     
-  else {tabs.open({
-    url: data.url("overlay.html").concat("?url=").concat(tabs.activeTab.url.replace("://","PSZP//")),
-    inBackground: false,
-    onReady: function(tab)
-    {
-      tab.attach({
-        contentScriptFile: data.url("app.js")
-      });
-    }
-  });
-}
 }
