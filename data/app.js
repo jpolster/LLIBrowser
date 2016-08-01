62db4bfe7a2a23c3b0269b70d0a392247e09e90f
Urmikl18 <pavel.shevchenko.95@gmail.com>
2016-08-01 10:23:45 +0200

    new regex, font reload

--- data/app.js
+++ data/app.js
@@ -212,7 +212,7 @@ this.oldURL = encodeURIComponent(page);
            console.log("Connection with server failed");
        });
     };
-    this.HTTP_REGEX = /^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/;
+    this.HTTP_REGEX = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
 }]);
 
 
@@ -243,7 +243,7 @@ this.oldURL = encodeURIComponent(page);
         cell1=row.insertCell(0);
         cell2=row.insertCell(1);
         cell1.innerHTML="Source";
-        cell2.innerHTML=lli["lom:identifier"]["dcterms:identifier"]["@id"];
+        cell2.innerHTML="<a href=\""+lli["lom:identifier"]["dcterms:identifier"]["@id"]+"\">"+lli["lom:identifier"]["dcterms:identifier"]["@id"]+"</a>";
 
         row=table.insertRow(3);
         cell1=row.insertCell(0);
